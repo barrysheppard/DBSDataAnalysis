b151a8bf33f8d672c3ea45f3c9ac06bc4ecac6db
Barry Sheppard <barrysheppard@gmail.com>
2018-09-25 20:43:27 +0100

    Adding notes from the Programming Tools course

---
+++ Tools/foods.py
@@ -0,0 +1,8 @@
+# Analysis on a foods dataset
+
+#import pandas as pd
+#df = pd.read_table('/Users/barrysheppard/Github/Tools/food.tsv')
+
+#df[:3]
+
+ls
---
+++ Tools/test.py
@@ -0,0 +1,7 @@
+# This is  my test file for
+
+import pandas as pd
+
+newDataframe = pd.dataframe()
+
+print(type(newDataframe))
